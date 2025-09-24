5869f01d1450600c491d613fd12ebd266062a3b5
Donovan Bale <donovan@balehaus.org>
2025-09-24 14:22:47 -0500

    beginning homepage

---
+++ src/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+            <span class="navbar-brand">
+                <h1>Hi, my name is Donovan! 🚀</h1>
+                <h4>Aspiring developer, writer, and artist.</h4>
+            </span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <div class="navbar-nav ms-auto  mb-2 mb-lg-0 n">
+                    <hr>
+                    <div class="nav-item ">
+                        <a class="nav-link active " aria-current="page" href="index.php">Home</a>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link " href="blog.php">Blog</a>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link " href="project.php">Projects</a>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link" href="#">My Writing</a>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link" href="https://myart.ewitsdonovan.com">My Artwork</a>
+                    </div>
+                    <div class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Me</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </nav>
+
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,58 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to Donovan’s Homepage</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+  <header>
+    <?php require_once('header.php') ?>
+  </header>
+  <main>
+    <div class="box">
+        <h2>My Domain</h2>
+        <p class="text-secondary">
+            I am a Student in Computer Science at <a href="https://www.mst.edu/">Missouri S&T</a>.
+            I am not an entrepeneur.
+        </p>
+        <p class="text-secondary">
+            I just wrapped up my fourth year on the World Champion <a href="https://marsrover.mst.edu/">Mars Rover Design Team</a>.
+            My first year I was a General Member of the Autonomy Subteam.
+            My second year I spent as the BaseStation & Signals Lead.
+            My third year I spent as the Software Architect.
+            And for my fourth and most recent year, I returned to a more General Member / Advising / Research role.
+            You can learn more about my time on the team <a href="marsrover.php">here</a>.
+        </p>
+        <p class="text-secondary">
+            In August 2025, I finished up my Software Development Internship at Nucor Corporation.
+        </p>
+        <p class="text-secondary">
+            I have had a Co-Op (June 2023 - January 2024) and an Internship (June 2024 - August 2025) at Hunter Engineering Company.
+        </p>
+        <p class="text-secondary">
+            I am a massive science fiction and fantasy fan. 
+            I aspire to one day be a writer comparable to the likes of Hobb, Jordan, or Erikson.
+            If you want to check out my writing, you can do so <a href="#">here</a>
+            (if I ever decide to post any of it). 
+        </p>
+        <p class="text-secondary">
+            Recently I have taken up drawing.
+            You can check out a web portfolio I made for a class
+            <a href="https://myart.ewitsdonovan.com">here</a>.
+        </p>
+        <p class="text-secondary">
+            Checkout my technical projects <a href="projects.php">here</a>
+            or on my <a href="https://github.com/UhOhDonovan">Github</a>.
+        </p>
+	</div>
+  </main>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
